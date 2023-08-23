01cc4ed5afdf7ac2f7427cc7978f0bfd4927d4bc
Mirza-Glitch <mirzaglitch@gmail.com>
2023-08-23 18:07:46 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Synthwave Theme
+
+Description: Experience the nostalgic charm like Synthwave theme of VSCode, now available for Acode Editor.
+
+![screenshot](screenshot.jpg)
+
+## Usage
+
+1. Open Acode Editor.
+2. Go to the `settings > Theme > Editor`.
+3. Select "Synthwave theme"
+4. Apply the theme.
+
+## Acknowledgement
+
+This extension was written by cloning [vscode-theme-extension](https://github.com/legendSabbir/acode-editorTheme-template), if you encounter any bugs or have any suggestions to make this theme even better then feel free to make a pull request on the [github repo](https://github.com/Mirza-Glitch/synthwave-acode-theme).
---
+++ src/main.js
@@ -0,0 +1,59 @@
+import plugin from '../plugin.json';
+import style from "./style.scss";
+const settings = acode.require('settings');
+const { editor } = editorManager
+
+const themeName = "synthwave-theme"
+
+ace.define(`ace/theme/${themeName}.css`, ["require", "exports", "module"], function (require, exports, module) { module.exports = style }),
+ace.define(`ace/theme/${themeName}`, ["require", "exports", "module", `ace/theme/${themeName}.css`, "ace/lib/dom"], function (require, exports, module) {
+  exports.isDark = !0,
+  exports.cssClass = `ace-${themeName}`,
+  exports.cssText = require(`./${themeName}.css`);
+  const dom = require("../lib/dom");
+  dom.importCssString(exports.cssText, exports.cssClass, !1)
+});
+(function () {
+  window.require(["ace/theme/" + themeName], function (m) {
+    if (typeof module == "object" && typeof exports == "object" && module) {
+      module.exports = m;
+    }
+  });
+})();
+
+class AcodePlugin {
+  async init() {
+    ace.require("ace/ext/themelist").themes.push({
+      caption: themeName.split("-").map(name => name[0].toUpperCase() + name.slice(1)).join(" "),
+      theme: "ace/theme/" + themeName,
+      isDark: true
+    });
+    
+    const currentTheme = settings.get("editorTheme")
+    if (currentTheme === themeName) editor.setTheme("ace/theme/" + themeName);
+    settings.on("update", this.onThemeChange)
+  }
+  
+  async destroy() { settings.off("update", this.onThemeChange) }
+  
+  onThemeChange(value) {
+    if (value === ("ace/theme/" + themeName)) {
+      editor.setTheme("ace/theme/" + themeName)
+      settings.update({ editorTheme: themeName })
+    }
+  }
+}
+
+if (window.acode) {
+  const acodePlugin = new AcodePlugin();
+  acode.setPluginInit(plugin.id, (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
+    acodePlugin.baseUrl = baseUrl;
+    acodePlugin.init($page, cacheFile, cacheFileUrl);
+  });
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
